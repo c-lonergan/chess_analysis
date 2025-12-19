e518ed8b411cde3a9f168dfa1bcbfc1ee2dbc0db
c-lonergan <charlielonergan@hotmail.com>
2025-12-19 16:24:06 +0000

    getting raw json data

--- chesscom_client.py
+++ chesscom_client.py
@@ -1,6 +1,8 @@
 """
 To-Do:
 - Handle Exception errors rather than just raise_for_status()
+- Custom date ranges
+- Do we need a class?
 """
 
 import os
@@ -31,37 +33,43 @@ class ChessComClient:
             timeout=TIMEOUT
         )
         r.raise_for_status()
-        return r.json()
+        return r
 
     def get_basic_info(self):
-        return self._get(self.base_url)
+        response = self._get(self.base_url).json()
+        return response
         
     def get_last_n_months_archives(self, n = MONTHS_BACK):
         url = f"{self.base_url}/games/archives"
-        archives = self._get(url)
+        archives = self._get(url).json()
         return sorted(archives['archives'])[-n:]
 
+    def make_archive_file_name(self, url: str) -> str:
+        arr = url.rsplit("/")
+        filename = f"games_{arr[-4]}_{arr[-2]}_{arr[-1]}.json"
+        return filename
+
     def download_archives(self):
-        raw_dir = Path(DATA_DIR) / "raw"
-        raw_dir.mkdir(parents=True, exist_ok=True)
+        raw_dir = os.path.join(DATA_DIR, "raw")
+        os.makedirs(raw_dir, exist_ok=True)
+
         archives = self.get_last_n_months_archives()
+        
         out_files = []
         for url in archives:
-            month_str = url.rsplit("/", 1)[-1]
-            out_path = raw_dir / f"games_{month_str}.json"
-            if out_path.exists():
+            filename = self.make_archive_file_name(url)
+            out_path = os.path.join(raw_dir, filename)
+            if os.path.isfile(out_path):
                 out_files.append(str(out_path))
                 continue
             r = self._get(url)
-            r.raise_for_status()
             with open(out_path, "w", encoding="utf-8") as f:
                 json.dump(r.json(), f)
             out_files.append(str(out_path))
+        
         return out_files
 
 if __name__ == "__main__":
     client = ChessComClient()
-    # info = client.get_basic_info()
-    # print(info)
     files = client.download_archives()
     print(files)
